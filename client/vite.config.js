31e3d3638b461263ec6518aa8cc3501922a91db6
vikash rajpoot <vikashraj1490@gmail.com>
2024-01-21 04:05:56 +0530

    render

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://one-infinity-server.vercel.app/',
+        target: 'https://oneinfinity.onrender.com/',
         secure: false,
       },
     },
