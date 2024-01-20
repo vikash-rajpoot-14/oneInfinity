642ff5cddfab940eb9cd09c46d7a9e07d387843f
vikash rajpoot <vikashraj1490@gmail.com>
2024-01-21 04:15:45 +0530

    deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://oneinfinity.onrender.com/',
-        secure: false,
-      },
-    },
-  },
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://oneinfinity.onrender.com/',
+//         secure: false,
+//       },
+//     },
+//   },
 
-  plugins: [react()],
-});
+//   plugins: [react()],
+// });
