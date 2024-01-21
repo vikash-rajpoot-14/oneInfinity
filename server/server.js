2ce34e0b8eb571ffcf4e20f9a51713c6ede977f2
vikash rajpoot <vikashraj1490@gmail.com>
2024-01-21 17:56:18 +0530

    ready to deploy

--- server/server.js
+++ server/server.js
@@ -21,10 +21,7 @@ mongoose
   const app = express();
   
 //middlewares
-app.use(cors({
-  origin: 'http://localhost:5173', 
-  credentials: true,
-}));
+app.use(cors());
 
 
 app.use(express.json());
